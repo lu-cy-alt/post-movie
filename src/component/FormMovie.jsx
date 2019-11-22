c2a9af19f1791faf0257fb3130394eef677510a0
Lucie Fontela <luciefontela@gmail.com>
2019-11-22 17:28:48 +0100

    formulaire end

--- src/component/FormMovie.jsx
+++ src/component/FormMovie.jsx
@@ -2,104 +2,102 @@ import React from 'react';
 import './FormMovie.css';
 
 
-
-class FormMovie  extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-     title: '',
-     poter: '',
-      comment: '',
-    }
-    this.onChange = this.onChange.bind(this);
-    this.submitForm = this.submitForm.bind(this);
+class FormMovie extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    title: '',
+    poster: '',
+    comment: '',
   }
+  this.onChange = this.onChange.bind(this);
+  this.submitForm = this.submitForm.bind(this);
 
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
+}
+
+onChange(e) {
+  this.setState({
+    [e.target.name]: e.target.value,
+  });
+}
+
+submitForm(e) {
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(this.state),
+  };
+
+  const url = 'https://post-a-form.herokuapp.com/api/movies/'
+
+  fetch(url, config)
+    .then(res => res.json())
+    .then(res => {
+      if (res.error) {
+        alert(res.error);
+      } else {
+        alert(`Submit!`);
+      }
+    }).catch(e => {
+      console.error(e);
+      alert('Error, something missing');
     });
-  }
-
-  submitForm(e) {
-    const config = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(this.state),
-      };
-      
-      const url = 'https://post-a-form.herokuapp.com/api/movies/'
-      
-      fetch(url, config)
-      .then(res => res.json())
-        .then(res => {
-          if (res.error) {
-            alert(res.error);
-          } else {
-            alert(`commentaire ajouté!`);
-          }
-        }).catch(e => {
-          console.error(e);
-          alert('Erreur lors d\'un commentaire');
-        });
-      
-  }
-
+  e.preventDefault();
+}
   
-
-
-  render(){
-    return(
-    <div>
-  <h1>Your favorite movie..</h1>
-
-  <form onSubmit={this.submitForm}>
-    <fieldset>
-      <div>
-        <label>Title</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          onChange={this.onChange}
-          value={this.state.title}
-        />
-      </div>
-
-      <div>
-        <label>url</label>
-        <input
-          type="text"
-          id="poster"
-          name="poster"
-          onChange={this.onChange}
-          value={this.state.poster}
-        />
-      </div>
-
-      <div>
-        <label>comment</label>
-        <textarea
-          type="text"
-          id="comment"
-          name="comment"
-          onChange={this.onChange}
-          value={this.state.comment}
-        />
-      </div>
-      <hr />
-      <div>
-        <input type="submit" value="Send" />
-      </div>
-    </fieldset>
-  </form>
+    render(){
+      return(
+<div>
+    <form className='container-form' onSubmit={this.submitForm}>
+      <fieldset>
+        <div>
+          <h1 className='first-title'>your favorite movie..</h1>
+            <label className='title'>Name </label>
+              <input
+                type="text"
+                className="partOfForm"
+                name="title"
+                onChange={this.onChange}
+                value={this.state.title}
+              />
+          </div>
+  
+          <div>
+            <label className='title'>Url</label>
+              <input
+              type="text"
+              className="partOfForm"
+              name="poster"
+              onChange={this.onChange}
+              value={this.state.poster}
+            />
+          </div>
+  
+          <div>
+            <label className='title'>Comment</label>
+              <textarea
+              type="text"
+              className="partOfForm"
+              name="comment"
+              onChange={this.onChange}
+              value={this.state.comment}
+              />
+          </div>
+          <hr />
+          <div>
+              <input type="submit" value="send" />
+          </div>
+      </fieldset>
+    </form>
 </div>
-    )
-}
+      )
+  }
+  
+  }
 
-}
 
 
-export default FormMovie ;
+
+  export default FormMovie;
